f8e9bac663eb3d57ce346219ee08beb66c8399fb
PD12pd <65124421+PD12pd@users.noreply.github.com>
2021-05-01 11:08:35 +0200

    Update rechner.py

--- rechner.py
+++ rechner.py
@@ -1,31 +1,37 @@
 # Taschenrechner
 while True:
   num1 = input("Gib die erste Zahl ein: ")
+  num11 = input("gib deine erste Zahl nach dem Komma ein: ")
   oper = input("Welche Rechenoperation soll durchgefuehrt werden? (+,-,/.,*): ")
   num2 = input("Gib die zweite Zahl ein: ")
+  num21 = input("Gib deine zweite Zahl nach dem Komma ein: ")
   
-  num1 = int(num1)
-  num2 = int(num2)
+  numA = float(str(num1) + "." + str(num11))
+  numB = float(str(num2) + "." + str(num21))
   
   if (oper == "+"):
-    print("Deine Rechnung:", num1, " + ", num2)
-    print("Ergebnis:", num1 + num2)
+    print("Deine Rechnung:", numA, " + ", numB)
+    print("Ergebnis:", numA + numB)
     
   elif(oper == "-"):
-      print("Deine Rechnung:", num1, " - ", num2)
-      print("Ergebnis:", num1 - num2)
+     print("Deine Rechnung:", numA, " - ", numB)
+     print("Ergebnis:", numA - numB)
     
   elif(oper == "/"):
-      print("Deine Rechnung:", num1, " / ", num2)
-      print("Ergebnis:", num1 / num2)
+     print("Deine Rechnung:", numA, " / ", numB)
+     print("Ergebnis:", numA / numB)
 
   elif(oper == "*"):
-      print("Deine Rechnung:", num1, " * ", num2)
-      print("Ergebnis:", num1 * num2)
+     print("Deine Rechnung:", numA, " * ", numB)
+     print("Ergebnis:", numA * numB)
   else:
-      print("Deine Eingaben sind nicht gueltig")
+     print("Deine Eingaben sind nicht gueltig")
 
-  jein = input("Willst du weiter rechnen? (Ja/Nein)")
+  jein = input("Willst du weiter rechnen? (ja/nein)")
 
-  if jein == "nein" or "ja" or "Nein" or "Ja":
-      print("Ok, du Kaspar nächste Rechnung")
+  if jein == "ja":
+     print("Das isch ja mega he. Nächste rechnung bitte!")
+  
+  else:
+    print("Ok tschüss du kaspar")
+    quit()
